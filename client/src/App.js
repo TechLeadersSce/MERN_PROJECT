0e915d98a0dfb88903b6435fab79ffa33f866dfd
JacobElbaz <jacobel@post.bgu.ac.il>
2022-03-11 15:18:06 +0200

    Add the Signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import LoginForm from './LoginForm';
 import './App.css';
+import { Routes, Route } from "react-router-dom"
+import SignupForm from './SignupForm';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const users = [{
+        email: "test@test.com",
+        password: "12345"
+    }]
+
+    const [user, setUser] = useState({ name: "", email: "" });
+    const [error, setError] = useState("");
+
+    const Login = details => {
+        console.log(details);
+        for (let user of users) {
+            if (details.email == user.email && details.password == user.password) {
+                console.log("Logged in");
+                setUser({
+                    name: details.name,
+                    email: details.email
+                });
+                break;
+            }
+        }
+        if(user.name == ""){
+            console.log(user.name);
+            console.log("Details not match!");
+            setError("Wrong password/email.")
+        }
+    }
+
+    const Signup = details => {
+        console.log(details);
+        if (details.password == details.confirmPass) {
+            console.log("Signed up");
+            const newUser = {email: details.email, password: details.password};
+            users.push(newUser);
+            
+        }
+        else {
+            console.log("Details not match!");
+            setError("Please confirm the password.")
+        }
+    }
+
+    return (
+        <div className="Login">
+            <Routes>
+                <Route path='/' element={<LoginForm Login={Login} error={error} setError={setError} />} />
+                <Route path='/signup' element={<SignupForm Signup={Signup} error={error} setError={setError}/>} />
+            </Routes>
+
+        </div>
+    );
 }
 
 export default App;
--- client/src/SignupForm.js
+++ client/src/SignupForm.js
@@ -1,28 +1,24 @@
 import React, {useState} from 'react';
-import './LoginForm.css';
+import './SignupForm.css';
 import {useNavigate} from 'react-router-dom'
 
-function LoginForm({Login, error, setError}) {
+function SignupForm({Signup, error, setError}) {
+
+    const navigate = useNavigate()
     
-    const [details, setDetails] = useState({name:"", email:"", password:""});
+    const [details, setDetails] = useState({email:"", password:"", confirmPass:""});
 
     const submitHandler = e => {
         e.preventDefault();
 
-        Login(details);
+        Signup(details);
     }
-
-    const navigate = useNavigate()
 
   return (
     <form onSubmit={submitHandler}>
         <div className='form-inner'>
-            <h2>Login</h2>
+            <h2>Signup</h2>
             {(error != "") ? (<div className="error">{error}</div>) : ""}
-            <div className='form-group'>
-                <label htmlFor="name">Name:</label>
-                <input type="text" name='name' id='name' onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
-            </div>
             <div className='form-group'>
                 <label htmlFor="email">Email:</label>
                 <input type="email" name='email' id='email' onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
@@ -31,11 +27,15 @@ function LoginForm({Login, error, setError}) {
                 <label htmlFor="password">Password:</label>
                 <input type="password" name='password' id='password' onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
             </div>
-            <input type="submit" value="LOGIN" />
-            <button className='signup' onClick={() => {navigate("/signup"); setError("")}}>Sign up</button>
+            <div className='form-group'>
+                <label htmlFor="password">Confirm password:</label>
+                <input type="password" name='confpass' id='confpass' onChange={e => setDetails({...details, confirmPass: e.target.value})} value={details.confirmPass}/>
+            </div>
+            <input type="submit" value="Sign Up" />
+            <button className='signup' onClick={() => {navigate("/"); setError("")}}>Back</button>
         </div>
     </form>
   )
 };
 
-export default LoginForm;
+export default SignupForm;
